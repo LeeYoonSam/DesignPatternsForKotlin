76ad886eadf0461d0be6dbd3a343121531539e10
LeeYoonSam <yoonsam86@gmail.com>
2022-10-04 11:20:34 +0900

    Adapter Pattern Cat, Dog, Fish 예제 추가

--- src/main/kotlin/structural/adapter/SimpleAnimalAdapterPattern.kt
+++ src/main/kotlin/structural/adapter/SimpleAnimalAdapterPattern.kt
@@ -3,3 +3,44 @@ package structural.adapter
 interface Animal {
     fun walk()
 }
+
+class Cat: Animal {
+    override fun walk() {
+        println("cat working")
+    }
+}
+
+class Dog: Animal {
+    override fun walk() {
+        println("dog working")
+    }
+}
+
+fun makeWalk(animal: Animal) {
+    animal.walk()
+}
+
+// 새로운 타입인 물고기, 걸을수 없음
+class Fish {
+    fun swim() {
+        println("fish swimming")
+    }
+}
+
+class FishAdapter(private val fish: Fish): Animal {
+    override fun walk() {
+        fish.swim()
+    }
+}
+
+fun main() {
+    val kitty = Cat()
+    val bingo = Dog()
+
+    makeWalk(kitty)
+    makeWalk(bingo)
+
+    val nimo = Fish()
+    val fishAdapter = FishAdapter(nimo)
+    makeWalk(fishAdapter)
+}
